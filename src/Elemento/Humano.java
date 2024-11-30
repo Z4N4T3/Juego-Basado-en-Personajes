359415832cd2d1fb1562c8c1e2bc25f9b14df6f1
jp <jurgenq75@gmail.com>
2024-11-30 11:27:51 -0600

    cambios en el metodo h.goplear

--- src/Elemento/Humano.java
+++ src/Elemento/Humano.java
@@ -1,5 +1,7 @@
 package Elemento;
 
+import java.util.Random;
+
 public class Humano extends Fisico implements Espiritual {
     private double inteligencia;
     private double fe;
@@ -27,11 +29,49 @@ public class Humano extends Fisico implements Espiritual {
     }
 
     public void conflictoMoral(Angeles a, Demonios d) {
-        // Lógica de conflicto moral
+        Random random = new Random();
+
+
+        int habilidad = random.nextInt(6);
+
+
+        double puntuacionAngel = a.luchar(this);
+        double puntuacionDemonio = d.luchar(this);
+
+
+        if (puntuacionAngel > puntuacionDemonio) {
+            this.bondad += 1;
+            this.maldad -= 1;
+        } else if (puntuacionDemonio > puntuacionAngel) {
+            this.bondad -= 1;
+            this.maldad += 1;
+        }
+
+        double efecto = golpear(puntuacionAngel > puntuacionDemonio ? a : d, habilidad);
+        if (efecto > 0) {
+            this.fe += 1;
+        } else {
+            this.fe -= 1;
+        }
     }
 
     public double golpear(Incorporeo s, int habilidad) {
-        return habilidad * 1.5; // Lógica de golpe
+        switch (habilidad) {
+            case 0:
+                return s.enganar(this);
+            case 1:
+                return s.guiar(this);
+            case 2:
+                return s.proteger(this);
+            case 3:
+                return s.tentar(this);
+            case 4:
+                return s.seducir(this);
+            case 5:
+                return s.probar(this);
+            default:
+                return 0;
+        }
     }
 
     @Override
